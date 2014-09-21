0e72480ef02a467ffdef0e2f1cd006a9e2535c66
Nico Weber <nicolasweber@gmx.de>
2014-09-21 16:10:54 -0700

    support explicit build order

--- src/graph.h
+++ src/graph.h
@@ -170,9 +170,10 @@ struct Edge {
 
   Edge()
       : rule_(NULL), pool_(NULL), dyndep_(NULL), env_(NULL), mark_(VisitNone),
-        id_(0), outputs_ready_(false), deps_loaded_(false),
-        deps_missing_(false), generated_by_dep_loader_(false),
-        implicit_deps_(0), order_only_deps_(0), implicit_outs_(0) {}
+        id_(0), run_time_ms_(0), critical_time_(0), outputs_ready_(false),
+        deps_loaded_(false), deps_missing_(false),
+        generated_by_dep_loader_(false), implicit_deps_(0),
+        order_only_deps_(0), implicit_outs_(0) {}
 
   /// Return true if all inputs' in-edges are ready.
   bool AllInputsReady() const;
@@ -198,6 +199,9 @@ struct Edge {
   // Append all edge explicit inputs to |*out|. Possibly with shell escaping.
   void CollectInputs(bool shell_escape, std::vector<std::string>* out) const;
 
+  uint64_t critical_time() const { return critical_time_; }
+  void set_critical_time(uint64_t critical_time) { critical_time_ = critical_time; }
+
   const Rule* rule_;
   Pool* pool_;
   std::vector<Node*> inputs_;
@@ -207,6 +211,8 @@ struct Edge {
   BindingEnv* env_;
   VisitMark mark_;
   size_t id_;
+  int run_time_ms_;
+  uint64_t critical_time_;
   bool outputs_ready_;
   bool deps_loaded_;
   bool deps_missing_;
